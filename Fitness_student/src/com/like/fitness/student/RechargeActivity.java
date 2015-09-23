50c63b24e982c45aa0df7125803cc7ea4c568a2c
joselyncui <15895026586@163.com>
2015-09-23 14:14:40 +0800

    绑定布局

--- Fitness_student/src/com/like/fitness/student/RechargeActivity.java
+++ Fitness_student/src/com/like/fitness/student/RechargeActivity.java
@@ -2,14 +2,33 @@ package com.like.fitness.student;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class RechargeActivity extends Activity {
+	private EditText mMoney;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_recharge);
+		mMoney = (EditText) findViewById(R.id.money);
+	}
+	
+	/**
+	 * 下一步 点击事件
+	 * @param view
+	 */
+	public void nextStep(View view) {
+		
 	}
 
-	
+	/**
+	 * 返回按钮
+	 * 
+	 * @param v
+	 */
+	private void back(View v) {
+		
+	}
 }
